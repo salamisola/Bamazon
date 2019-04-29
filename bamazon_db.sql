80b84464943ced3fe861504f6d103e3109fdb20e
solasalami <salamisola@gmail.com>
2019-04-29 17:02:12 -0400

    database table and columns created

--- bamazon_db.sql
+++ bamazon_db.sql
@@ -5,6 +5,10 @@ CREATE TABLE products (
 item_id INTEGER(20) NOT NULL auto_increment,
 product_name VARCHAR(100) NOT NULL,
 department_name VARCHAR(100) NOT NULL,
+price INTEGER(10) NOT NULL,
+stock_quantity INTEGER(30) NOT NULL,
+PRIMARY KEY (id)
+);
 
 
 
